3d565ad06293eac38230aab43ffe07d4987ca5bb
Emad Emami <emad.emami.68@gmail.com>
2019-12-29 12:29:43 -0800

    changed

---
+++ packages/client/src/components/GameBoardCanvas/index.jsx
@@ -0,0 +1,64 @@
+import React, { useCallback, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+function GameBoardCanvas({
+  data,
+  width = 800,
+  height = 800,
+  radius = 8,
+  style,
+  className,
+}) {
+  const [ctx, setCtx] = useState();
+  const getRefCallback = useCallback(el => setCtx(el.getContext('2d')), []);
+  useEffect(() => {
+    if (ctx && data.length) {
+      const ratio = width / data[0].length;
+      ctx.clearRect(0, 0, width, height);
+      for (let i = 0; i < data.length; i += 1) {
+        const row = data[i];
+        for (let j = 0; j < row.length; j += 1) {
+          const cell = row[j];
+          ctx.beginPath();
+          ctx.arc(
+            i * ratio + ratio / 2,
+            j * ratio + ratio / 2,
+            radius,
+            0,
+            2 * Math.PI,
+          );
+          ctx.strokeStyle = '#c114c1';
+          ctx.lineWidth = 0.2;
+          ctx.stroke();
+          if (cell) {
+            ctx.fillStyle = '#c114c1';
+            ctx.fill();
+          }
+        }
+      }
+    }
+  }, [ctx, width, height, data, radius]);
+
+  return (
+    <canvas
+      ref={getRefCallback}
+      width={width}
+      height={height}
+      style={style}
+      className={className}
+    />
+  );
+}
+
+GameBoardCanvas.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.number, PropTypes.bool])),
+  ).isRequired,
+  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  style: PropTypes.shape({}),
+  className: PropTypes.string,
+  radius: PropTypes.number,
+};
+
+export default GameBoardCanvas;
--- packages/client/src/pages/GamePage/index.jsx
+++ packages/client/src/pages/GamePage/index.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { random } from 'lodash-es';
+import GameBoardCanvas from 'components/GameBoardCanvas';
+
+function createRandomGeneration(width = 40, height = 40) {
+  const generation = new Array(width);
+  for (let i = 0; i < generation.length; i += 1) {
+    generation[i] = [];
+    for (let j = 0; j < height; j += 1) {
+      generation[i][j] = random(0, 1, false);
+    }
+  }
+  return generation;
+}
+const cells = createRandomGeneration();
 
 function Game() {
-  return <div>Game</div>;
+  return <GameBoardCanvas data={cells} />;
 }
 
 export default Game;
